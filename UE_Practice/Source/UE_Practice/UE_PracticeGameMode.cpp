94e7a857d7e8cd720b24d3055c645fd2514ab7cc
Gioele Giusti <gioele.pg@gmail.com>
2023-10-27 16:56:28 +0200

    add new character, add new animations, create new game mode,

--- UE_Practice/Source/UE_Practice/UE_PracticeGameMode.cpp
+++ UE_Practice/Source/UE_Practice/UE_PracticeGameMode.cpp
@@ -7,7 +7,7 @@
 AUE_PracticeGameMode::AUE_PracticeGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_MarkerMan"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
